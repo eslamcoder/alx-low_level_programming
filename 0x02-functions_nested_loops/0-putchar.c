9723b3de82988d8a47cec23eeb46b3b208b4bb73
eslamcoder <eslam2photo@gmail.com>
2023-03-21 13:52:29 -0700

    Fix task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,14 @@
+#include  <main.h>
 #include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Print _putchar
+ * Is writting by mbah
+ * Return 0
  */
-int _putchar(char c)
+
+int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,5 +1,5 @@
+#include <main.h>
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,15 +9,5 @@
  */
 int _putchar(char c)
 {
-        _putchar('-');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
-
+	return (write(1, &c, 1));
 }
